992ae3ae61b5091df7acd4819938ee321674650d
Vaibhav Kumar <vaibhavaol201484@gmail.com>
2024-09-16 15:06:04 -0400

    initial commit changing username and id

--- write_ids.c
+++ write_ids.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
 
 int main() {
-    char GITHUB_ID[] = "Fill_in_Your_Github_Id";
-    char STUDENT_ID[] = "Fill_in_Your_Student_Id";
+    char GITHUB_ID[] = "vbhk997";
+    char STUDENT_ID[] = "923013252";
 
     return 0 ;
 }
